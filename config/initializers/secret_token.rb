4b27658fa9201373616f7d9aa38f614cd6d90cf2
Neo4j-Ubuntu64 <paolodipietro84@gmail.com>
2013-11-29 12:38:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f3dbc5bae70dc535b58c5220b339c15fbd47f0775de0893eb989661b34847c6d37bde370c7ca723b2d9c369dad5e54715a2b6623e71ecd497bfc761ac003100d'
